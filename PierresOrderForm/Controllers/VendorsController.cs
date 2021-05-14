602f24a562e287eac35f9965f85ea2191368983e
“Jamie <knutsenjamie@yahoo.com>
2021-05-14 16:18:01 -0700

    begin writing VendorsController.cs

--- PierresOrderForm/Controllers/VendorsController.cs
+++ PierresOrderForm/Controllers/VendorsController.cs
@@ -22,38 +22,21 @@ namespace PierresOrderForm.Controllers
     }
 
     [HttpPost("/vendors")]
-    public ActionResult Create(string vendorsName, string vendordescription)
+    public ActionResult Create(string vendorsName, string vendorDescription)
     {
-      Vendors newVendors = new Vendors(categoryName);
+      Vendors newVendors = new Vendors(vendorsName, vendorDescription);
       return RedirectToAction("Index");
     }
 
-    [HttpGet("/categories/{id}")]
+    [HttpGet("/vendors/{id}")]
     public ActionResult Show(int id)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
-      Category selectedCategory = Category.Find(id);
-      List<Item> categoryItems = selectedCategory.Items;
-      model.Add("category", selectedCategory);
-      model.Add("items", categoryItems);
+      Vendors selectedVendors = Vendors.Find(id);
+      List<Orders> vendorsOrders = selectedVendors.Orders;
+      model.Add("Vendor", selectedVendors);
+      model.Add("Order", vendorsOrders);
       return View(model);
     }
-
-
-    // This one creates new Items within a given Category, not new Categories:
-
-    [HttpPost("/categories/{categoryId}/items")]
-    public ActionResult Create(int categoryId, string itemDescription)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Category foundCategory = Category.Find(categoryId);
-      Item newItem = new Item(itemDescription);
-      foundCategory.AddItem(newItem);
-      List<Item> categoryItems = foundCategory.Items;
-      model.Add("items", categoryItems);
-      model.Add("category", foundCategory);
-      return View("Show", model);
-    }
-
-  }
+   }
 }
