2d632824ec05056d00e6b8d26f9422adf3b5228c
“Jamie <knutsenjamie@yahoo.com>
2021-05-14 11:30:03 -0700

    pass OrdersDescription property check test

--- PierresOrderForm.Tests/ModelTests/OrderTests.cs
+++ PierresOrderForm.Tests/ModelTests/OrderTests.cs
@@ -24,5 +24,19 @@ namespace PierresOrderForm.Tests
             string result = newOrders.OrdersTitle;
             Assert.AreEqual(orderstitle, result);
         }
+        [TestMethod]
+        public void GetOrdersDescription_ReturnsOrdersDescription_String()
+        {
+            string orderstitle = "Joe's Cafe Order";
+            string ordersdescription = "Joe's Cafe Order Invoice For x50 Pan Au Chocolat and x120 Traditional Croissants";
+            string ordersdate = "05/14/2021"; 
+            string ordersprice = "$185.50";
+            
+            Orders newOrders =  new Orders(orderstitle, ordersdescription, ordersdate, ordersprice);
+            string result = newOrders.OrdersDescription;
+            Assert.AreEqual(ordersdescription, result);
+        }
+        
+
     }
 }
