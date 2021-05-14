75356e171f7f5d9fe438e0f9fadbfea88ceb6e91
“Jamie <knutsenjamie@yahoo.com>
2021-05-14 09:22:36 -0700

    get good fail for vendorsname property test

--- PierresOrderForm.Tests/ModelTests/VendorsTests.cs
+++ PierresOrderForm.Tests/ModelTests/VendorsTests.cs
@@ -12,5 +12,13 @@ namespace PierresOrderForm.Tests
             Vendors newVendors = new Vendors();
             Assert.AreEqual(typeof(Vendors), newVendors.GetType());
         }
+        [TestMethod]
+        public void GetVendorsName_ReturnsVendorsName_String()
+        {
+            string vendorsname = "Joe's Ingredient Warehouse";
+            Vendors newVendors =  new Vendors(vendorsname);
+            string result = newVendors.VendorsName;
+            Assert.AreEqual(vendorsname, result);
+        }
     }
 }
--- PierresOrderForm/Models/Vendors.cs
+++ PierresOrderForm/Models/Vendors.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 namespace PierresOrderForm.Models 
 {
     public class Vendors
     {
-        
+        public string VendorsName { get; }
+        public Vendors(string vendorsname)
+        {
+            VendorsName = vendorsname;
+        }
     }
 }
